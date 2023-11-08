2d9c9c85791ae1b12a4ca52b17507b2bebdfb9e1
NathanPerezR <88894841+NathanPerezR@users.noreply.github.com>
2023-11-07 20:57:03 -0600

    cleaned up stytling, added in generator

--- degreeAudit.pl
+++ degreeAudit.pl
@@ -1,184 +1,121 @@
-% s(casp) programming
+% 
+
+
+% s(CASP) Programming
 :- use_module(library(scasp)).
-% uncomment to suppress warnings
+% Uncomment to suppress warnings
 %:- style_check(-discontiguous).
 %:- style_check(-singleton).
 %:- set_prolog_flag(scasp_unknown, fail).
 
-% todo: still need to account for aps
-% todo: still need to account for extra hours
-% todo: can likely adjust the core to make it easier to aduit, hours dont matter, the things at the bottom do
-% todo: y is a prereq of x
-% todo: y is a pre or corequiste of x
-
+% peoples name
+person(nathan).
+ 
 % domains
-cs(1200).
-cs(1337).
-cs(2305).
-cs(2340).
-cs(3162).
-cs(3305).
-cs(3341).
-cs(3345).
-cs(3354).
-cs(3377).
-cs(4141).
-cs(4337).
-cs(4341).
-cs(4347).
-cs(4348).
-cs(4349).
-cs(4384).
-cs(4485).
-cs(43xx).
-cs(43yy).
+class_type(cs).
+class_type(math).
+class_type(phys).
+class_type(ams).
+class_type(huma).
+class_type(lit).
+class_type(phil).
+class_type(psy).
+class_type(govt).
+class_type(rhet).
+class_type(econ).
+class_type(geog).
+class_type(crim).
+class_type(film).
+class_type(ahst).
+class_type(art).
+class_type(musi).
+class_type(thea).
 
+  class(cs, 1200).
+  class(cs, 1337).
+  class(cs, 2305).
+  class(cs, 2340).
+  class(cs, 3162).
+  class(cs, 3305).
+  class(cs, 3341).
+  class(cs, 3345).
+  class(cs, 3354).
+  class(cs, 3377).
+  class(cs, 4141).
+  class(cs, 4337).
+  class(cs, 4341).
+  class(cs, 4347).
+  class(cs, 4348).
+  class(cs, 4349).
+  class(cs, 4384).
+  class(cs, 4485).
+  class(cs, 4300). % any 43XX, can not be a class listed above
+  class(cs, 4300). % any 43XX, can not be a class listed above
+class(math, 2413).
+class(math, 2414).
+class(math, 2417).
+class(math, 2418).
+class(math, 2419).
+class(phys, 2125).
+class(phys, 2126).
+class(phys, 2325).
+class(phys, 2326).
+ class(ams, 2300).
+ class(ams, 2341).
+class(huma, 1301).
+ class(lit, 2331).
+class(phil, 1301).
+class(phil, 2316).
+class(phil, 2317).
+ class(psy, 2301).
+class(govt, 2305).
+class(govt, 2306).
+class(rhet, 1302).
+class(econ, 2301).
+class(econ, 2302).
+class(geog, 2303).
+class(crim, 1301).
+class(film, 2332).
+class(ahst, 1301).
+class(ahst, 1304).
+class(ahst, 2331).
+ class(art, 1301).
+class(musi, 1306).
+class(thea, 1310).
 
-math(2413).
-math(2414).
-math(2417).
-math(2418).
-math(2419).
+% def of what it means to be in a class
+in(X, Y, Z) :- person(X), class(Y, Z).
 
-phys(2125).
-phys(2126).
-phys(2325).
-phys(2326).
+% ----freshmen_year-----
 
-ams(2300).
-ams(2341).
-
-huma(1301).
-
-lit(2331).
-
-phil(1301).
-phil(2316).
-phil(2317).
-
-psy(2301).
-
-govt(2305).
-govt(2306).
-
-rhet(1302).
-
-econ(2301).
-econ(2302).
-
-geog(2303).
-
-crim(1301).
-
-film(2332).
-
-ahst(1301).
-ahst(1304).
-ahst(2331).
-
-art(1301).
-
-musi(1306).
-
-thea(1310).
-
-freshmen_year(X) :-
-
-  %TODO need to find a way of doing the following:
-    X has taken all classes in the domain of Y, is there a better way then this
-
-% freshmen_year-----
-
-                      ecs_1100( ).
-                       cs_1200( ).
-     m2413_2417() :- math_2413( ). % must have one or the other
-     m2413_2417() :- math_2417( ). % must have one or the other
-                       cs_1337( ).
-                    corehours6( ).
-                     univ_1010( ).
-
-                  cs_2336_2337( ).
-                       cs_2305( ).
-                math_2414_2419( ).
-                     phys_2325( ).
-                     phys_2125( ).
 %-----sophmore_year------
-                       cs_3305( ).
-                       cs_2340( ).
-                     phys_2326( ).
-                     phys_2126( ).
-                        core_6( ).
 
-                       cs_3341( ).
-                       cs_3345( ).
-                       cs_3377( ).
-                     math_2418( ).
-                        core_6( ).
 %-----junior_year--------
-                       cs_3162( ).
-                       cs_4348( ).
-                       cs_4349( ).
-                         core3( ).
-                         free1( ).
-                         
-                       cs_3354( ).
-                       cs_4337( ).
-                       cs_4341( ).
-                       cs_4141( ).
-                      ecs_3390( ).
-                         core3( ).
+
 %-----senior_year-------
-                       cs_4384( ).
-                       cs_43xx( ).
-                       cs_43xx( ).
-
-                       cs_4347( ).
-                         free6( ).
-
-                       cs_4485( ).
-                       cs_43xx( ).
-                         free6( ).
-
 
 %-----core---------------------------------------------------
-three_hour_arts(X) :- 
-    ahst_1301(X)
-  ; ahst_1304(X)
-  ; ahst_2331(X)
-  ;  art_1301(X)
-  ; film_2332(X)
-  ; musi_1306(X)
-  ; thea_1310(X).
 
-three_hours_humanities(X) :-  
-     ams_2300(X)
-  ;  ams_2341(X)
-  ; huma_1301(X)
-  ;  lit_2331(X)
-  ; phil_1301(X)
-  ; phil_2316(X)
-  ; phil_2317(X).
+%-----script used to create facts for saying someone has taken a class----------------------------------------
+generate_facts_for_x(X) :- % Replace with the actual persons name when calling
+Courses = [cs(1301), cs(1200), cs(1337), cs(2305), cs(2340), cs(3162), cs(3305), cs(3341), cs(3345), cs(3354) , cs(3377), cs(4141), cs(4337), cs(4341), cs(4347), cs(4348), cs(4349), cs(4384), cs(4485), cs(4300), cs(4300) 
+              ,math(2413) ,math(2414) ,math(2417) ,math(2418) ,math(2419) ,phys(2125) ,phys(2126) ,phys(2325) ,phys(2326) ,ams(2300) ,ams(2341) ,huma(1301) ,lit(2331) ,phil(1301) ,phil(2316) ,phil(2317) ,psy(2301) ,govt(2305) 
+              ,govt(2306) ,rhet(1302) ,econ(2301) ,econ(2302) ,geog(2303) ,crim(1301) ,film(2332) ,ahst(1301) ,ahst(1304) ,ahst(2331) ,art(1301) ,musi(1306) ,thea(1310)],    
+generate_facts(X, Courses).
 
-% TODO
-six_hours_history(X) :- .
+% generates a list of facts in the format in(NAME, COURSETYPE(ID))
+generate_facts(_, []).
+generate_facts(X, [Course | Rest]) :-
+    format("in(~w, ~w).~n", [X, Course]),
+    generate_facts(X, Rest).
 
-three_hours_social_science(X) :- 
-    crim_1301(X)
-  ; econ_2301(X)
-  ; econ_2302(X)
-  ; geog_2303(X)
-  ;  psy_2301(X).
+%%%---------------------------------------------------------------------------------------
 
-rhet_1302( ).
-
-government(X) :- 
-    govt_2305(X) 
-  ; govt_2306(X).
-%-----core---------------------------------------------------
+% final script used to audit degrees ---------------------------------------------------------
 
 
 
 
-/** <ex> your ex queries go here, e.g.
-?- ? p( ).
+/** <ex> To make a list of facts for saying 'test' is in the courses listed in the generate_facts_for_x function 
+ * generate_facts_for_x(test).
 */
